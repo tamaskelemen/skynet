564770b5ffdab223d4b50852a4e03905ff619db7
Adam Debreceni <adam.debreceni@transferwise.com>
2020-10-03 14:49:34 +0200

    mongo connection and docker

--- src/main/java/com/skynet/ServletInitializer.java
+++ src/main/java/com/skynet/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.skynet;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
