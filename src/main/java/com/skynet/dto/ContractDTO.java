4d47533b8bcd6d81e977661bde1011a6dc16bfdf
Adam Debreceni <adam.debreceni@transferwise.com>
2020-10-04 19:50:59 +0200

    aggregate

--- src/main/java/com/skynet/dto/ContractDTO.java
+++ src/main/java/com/skynet/dto/ContractDTO.java
@@ -3,43 +3,25 @@ package com.skynet.dto;
 import java.util.Date;
 
 public class ContractDTO {
-	private String description;
-	private String price;
-	private Date startDate;
-	private Date endDate;
+	private String[] description;
+	private Double price;
 
 	public ContractDTO() {
 	}
 
-	public String getDescription() {
+	public String[] getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
+	public void setDescription(String[] description) {
 		this.description = description;
 	}
 
-	public String getPrice() {
+	public Double getPrice() {
 		return price;
 	}
 
-	public void setPrice(String price) {
+	public void setPrice(Double price) {
 		this.price = price;
-	}
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
 	}
 }
